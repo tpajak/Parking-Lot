cc7ba0f270af795218e9912fa245db1b48405d9e
Tomasz Pająk <tomasz@pajak.name>
2022-05-06 06:25:39 +0200

    removing non-null assertion when readying

--- Parking Lot/task/src/parking/Utils.kt
+++ Parking Lot/task/src/parking/Utils.kt
@@ -2,7 +2,6 @@ package parking
 
 class Utils {
     companion object {
-//        fun readUserInput() = readLine()!!.split(" ")
         fun readUserInput() = readLine()?.let {
             it.split(" ")
         } ?: "-1" as List<String>
