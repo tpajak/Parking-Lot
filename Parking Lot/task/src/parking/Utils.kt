18ee6d080fc3747c6298aacbd24844b7f4e017d4
Tomasz Pająk <tomasz@pajak.name>
2022-05-06 06:24:38 +0200

    removing non-null assertion when reading user input

--- Parking Lot/task/src/parking/Utils.kt
+++ Parking Lot/task/src/parking/Utils.kt
@@ -2,6 +2,9 @@ package parking
 
 class Utils {
     companion object {
-        fun readUserInput() = readLine()!!.split(" ")
+//        fun readUserInput() = readLine()!!.split(" ")
+        fun readUserInput() = readLine()?.let {
+            it.split(" ")
+        } ?: "-1" as List<String>
     }
 }
